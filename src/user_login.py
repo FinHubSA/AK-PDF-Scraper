c7718f0c15174198e58e7f6173a0531814a0eb08
DanaeBouwer <danae.bouwer@gmail.com>
2023-03-01 09:02:02 +0200

    Refactored and tested user_login

--- src/user_login.py
+++ src/user_login.py
@@ -1,333 +1,22 @@
 import time
-import os
 import emoji
+import os
 
 from termcolor import colored
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
+from src.errors import MainException, TypoException
 
 from src.helpers import system, typo
 
 is_windows = system()
 
 
-def get_login_method():
-
-    print(
-        "\n"
-        + colored(" i ", "blue", attrs=["reverse"]) * (is_windows)
-        + emoji.emojize(":information:") * (not is_windows)
-        + "   Please follow the prompts below to login."
-    )
-
-    login_method = input(
-        colored(
-            "\n-- Type [1] to login via institution VPN or wifi"
-            + "\n-- Type [2] to manually login via the JSTOR website"
-            + "\n-- Type [3] to return to main menu"
-            + "\n   : ",
-        )
-    ).strip()
-
-    if login_method == "1" or login_method == "2":
-        return login_method
-
-    elif login_method == "3":
-        return login_method
-
-    else:
-        typo()
-
-        return get_login_method()
-
-
-def login(driver, login_method):
-
-    if login_method == "1":
-        return vpn_login(driver)
-
-    if login_method == "2":
-        return manual_login(driver)
-
-
-def vpn_login(driver):
-
-    print(
-        "\n"
-        + (colored(" i ", "blue", attrs=["reverse"])) * (is_windows)
-        + (emoji.emojize(":information:")) * (not is_windows)
-        + "   You have chosen to login via VPN/wifi."
-    )
-
-    time.sleep(1)
-
-    print(
-        "\n"
-        + colored(" i ", "blue", attrs=["reverse"]) * (is_windows)
-        + emoji.emojize(":information:") * (not is_windows)
-        + "   Follow the steps below:"
-    )
-
-    time.sleep(1)
-
-    print(
-        colored(
-            "\n\nStep 1/1: Please connect to your institution's VPN or wifi, then continue.\n",
-            "blue",
-        )
-    )
-
-    time.sleep(1)
-
-    cont = proceed()
-
-    if cont == "1":
-
-        print("\nGive it a second, we are checking for successful login.\n")
-
-        driver.get("https://www.jstor.org/")
-
-        try:
-
-            WebDriverWait(driver, 60).until(
-                expected_conditions.element_to_be_clickable(
-                    (
-                        By.CLASS_NAME,
-                        "query-builder-input-group",
-                    )
-                )
-            )
-
-        except:
-            print(
-                "\n"
-                + colored(" ! ", "red", attrs=["reverse"]) * (is_windows)
-                + emoji.emojize(":red_exclamation_mark:") * (not is_windows)
-                + colored("  Unable to load JSTOR page.\n", "red")
-            )
-
-            print(
-                "\n"
-                + colored(" i ", "blue", attrs=["reverse"]) * (is_windows)
-                + emoji.emojize(":information:") * (not is_windows)
-                + "  Check your internet connection and try again.\n"
-            )
-
-            driver.close()
-
-            return False
-
-        time.sleep(2)
-
-        try:
-            driver.find_element(By.CLASS_NAME, "pds__access-provided-by")
-
-            time.sleep(1)
-
-            print(
-                "\n"
-                + colored(" ! ", "green", attrs=["reverse"]) * (is_windows)
-                + emoji.emojize(":check_mark_button:") * (not is_windows)
-                + colored("  Login was successful!\n", "green")
-            )
-
-            time.sleep(1)
-
-            driver.maximize_window()
-
-        except:
-
-            time.sleep(1)
-
-            print(
-                "\n"
-                + colored(" ! ", "red", attrs=["reverse"]) * (is_windows)
-                + emoji.emojize(":red_exclamation_mark:") * (not is_windows)
-                + colored("  Login was unsuccessful\n", "red")
-            )
-
-            return False
-    else:
-
-        return False
-
-    return True
-
-
-def manual_login(driver):
-
-    print(
-        "\n"
-        + colored(" i ", "blue", attrs=["reverse"]) * (is_windows)
-        + emoji.emojize(":information:") * (not is_windows)
-        + "   You will be prompted to manually login via the JSTOR website."
-    )
-
-    time.sleep(1)
-
-    cont = proceed()
-
-    if cont == "1":
-
-        print("\nYou are now being routed to JSTOR home page.")
-
-        time.sleep(2)
-
-        print("\nSit tight and wait for Google Chrome to open on your screen.\n")
-
-        time.sleep(2)
-
-        print(
-            "\n"
-            + (colored(" i ", "blue", attrs=["reverse"])) * (is_windows)
-            + (emoji.emojize(":information:")) * (not is_windows)
-            + "   While the browser opens, read through the login steps:"
-        )
-
-        time.sleep(1)
-
-        print(
-            colored(
-                "\nStep 1/4: Navigate to the top of the JSTOR home page, and click on the link: ",
-                "blue",
-            )
-            + colored(
-                "Log in through your library.",
-                "blue",
-                attrs=["reverse"],
-            )
-            * (is_windows)
-            + colored(
-                "Log in through your library.",
-                "blue",
-                attrs=["bold"],
-            )
-            * (not is_windows)
-            + colored(
-                "\nStep 2/4: Search for your institution by using the search box.\nStep 3/4: Log in using your institution's login credentials.\nStep 4/4: Accept the cookies.",
-                "blue",
-            )
-        )
-
-        time.sleep(1)
-
-        print(
-            "\nGive it a second, we are checking if the page has loaded successfully."
-        )
-
-        driver.get("https://www.jstor.org/")
-
-        try:
-
-            WebDriverWait(driver, 60).until(
-                expected_conditions.element_to_be_clickable(
-                    (By.CLASS_NAME, "query-builder-input-group")
-                )
-            )
-        except:
-            print(
-                "\n"
-                + colored(" ! ", "red", attrs=["reverse"]) * (is_windows)
-                + emoji.emojize(":red_exclamation_mark:") * (not is_windows)
-                + colored("  Unable to load JSTOR page.\n", "red")
-            )
-
-            print(
-                "\n"
-                + colored(" i ", "blue", attrs=["reverse"]) * (is_windows)
-                + emoji.emojize(":information:") * (not is_windows)
-                + "   Check your internet connection and try again.\n"
-            )
-
-            driver.close()
-
-            return False
-
-        driver.maximize_window()
-
-        print(
-            "\n"
-            + colored(" i ", "blue", attrs=["reverse"]) * (is_windows)
-            + emoji.emojize(":information:") * (not is_windows)
-            + "   Once you have completed the steps, continue:"
-        )
-
-        cont = proceed()
-
-        if cont == "1":
-
-            try:
-
-                WebDriverWait(driver, 60).until(
-                    expected_conditions.element_to_be_clickable(
-                        (By.CLASS_NAME, "query-builder-input-group")
-                    )
-                )
-
-            except:
-                print(
-                    "\n"
-                    + colored(" ! ", "red", attrs=["reverse"]) * (is_windows)
-                    + emoji.emojize(":red_exclamation_mark:") * (not is_windows)
-                    + colored("  Unable to load JSTOR page.\n", "red")
-                )
-
-            print("\nChecking for successful login.\n")
-
-            time.sleep(1)
-
-            try:
-                driver.find_element(By.CLASS_NAME, "pds__access-provided-by")
-                time.sleep(1)
-                print(
-                    "\n"
-                    + colored(" ! ", "green", attrs=["reverse"]) * (is_windows)
-                    + emoji.emojize(":check_mark_button:") * (not is_windows)
-                    + colored("  Login was successful!\n", "green")
-                )
-
-                time.sleep(1)
-
-                driver.maximize_window()
-
-                driver.set_window_position(-2024, 2024)
-
-            except:
-                time.sleep(1)
-                print(
-                    "\n"
-                    + colored(" ! ", "red", attrs=["reverse"]) * (is_windows)
-                    + emoji.emojize(":red_exclamation_mark:") * (not is_windows)
-                    + colored("  Login was unsuccessful\n", "red")
-                )
-
-                return False
-        else:
-            return False
-    else:
-        return False
-
-    return True
-
-
-def proceed():
-
-    proceed_input = input(
-        colored(
-            "\n-- Type [1] to continue\n-- Type [2] to return to contributions menu\n   : "
-        )
-    )
-
-    if proceed_input != "1" and proceed_input != "2":
-        typo()
-
-        return proceed()
-
-    return proceed_input
-
-
-def login_requirements():
+def print_login_requirements():
 
     is_windows = system()
 
@@ -381,7 +70,7 @@ def login_requirements():
         "\n• Close the Google Chrome window that will be opened in the next steps.\n• Interfere with the Google Chrome window unless prompted to do so."
     )
 
-    input(
+    get_input(
         colored("\n\n-- Press ")
         + colored("ENTER/RETURN", attrs=["reverse"]) * (is_windows)
         + colored("ENTER/RETURN", attrs=["bold"]) * (not is_windows)
@@ -389,9 +78,7 @@ def login_requirements():
     )
 
 
-def login_instructions():
-
-    time.sleep(1)
+def print_login_instructions():
 
     print(
         "\n\n\n"
@@ -432,12 +119,294 @@ def login_instructions():
     )
 
 
-def end_program():
+def receive_login_action():
 
-    exit_program = input(
+    print(
+        "\n"
+        + colored(" i ", "blue", attrs=["reverse"]) * (is_windows)
+        + emoji.emojize(":information:") * (not is_windows)
+        + "   Please follow the prompts below to login."
+    )
+
+    login_method = get_input(
         colored(
-            "\n-- Type [1] to exit\n-- Type [2] to make another contribution\n   : "
+            "\n-- Type [1] to login via institution VPN or wifi"
+            + "\n-- Type [2] to manually login via the JSTOR website"
+            + "\n-- Type [3] to return to main menu"
+            + "\n   : ",
         )
-    ).strip()
+    )
 
-    return exit_program
+    return login_method
+
+
+def vpn_login(driver, url, html_load, html_login):
+
+    print(
+        "\n"
+        + colored(" i ", "blue", attrs=["reverse"]) * (is_windows)
+        + emoji.emojize(":information:") * (not is_windows)
+        + "   Follow the steps below:"
+    )
+
+    time.sleep(1)
+
+    print(
+        colored(
+            "\n\nStep 1/1: Please connect to your institution's VPN or wifi, then continue.\n",
+            "blue",
+        )
+    )
+
+    time.sleep(1)
+
+    cont = receive_proceed_action()
+
+    if cont == "1":
+
+        print("\nGive it a second, we are checking for successful login.\n")
+
+        driver.get(url)
+
+        if validate_page_load(driver, html_load) == False:
+            return False
+
+        time.sleep(2)
+
+        if validate_login(driver, html_login) == True:
+
+            driver.maximize_window()
+
+            return True
+
+        else:
+
+            return False
+
+    elif cont == "2":
+
+        driver.close()
+
+        return False
+
+
+def manual_login(driver, url, html_load, html_login):
+
+    print("\nYou are now being routed to JSTOR home page.")
+
+    time.sleep(2)
+
+    print("\nSit tight and wait for Google Chrome to open on your screen.\n")
+
+    time.sleep(2)
+
+    print(
+        "\n"
+        + (colored(" i ", "blue", attrs=["reverse"])) * (is_windows)
+        + (emoji.emojize(":information:")) * (not is_windows)
+        + "   While the browser opens, read through the login steps:"
+    )
+
+    time.sleep(1)
+
+    print(
+        colored(
+            "\nStep 1/4: Navigate to the top of the JSTOR home page, and click on the link: ",
+            "blue",
+        )
+        + colored(
+            "Log in through your library.",
+            "blue",
+            attrs=["reverse"],
+        )
+        * (is_windows)
+        + colored(
+            "Log in through your library.",
+            "blue",
+            attrs=["bold"],
+        )
+        * (not is_windows)
+        + colored(
+            "\nStep 2/4: Search for your institution by using the search box.\nStep 3/4: Log in using your institution's login credentials.\nStep 4/4: Accept the cookies.",
+            "blue",
+        )
+    )
+
+    time.sleep(1)
+
+    print("\nGive it a second, we are checking if the page has loaded successfully.")
+
+    driver.get(url)
+
+    if validate_page_load(driver, html_load) == False:
+        return False
+
+    driver.maximize_window()
+
+    print(
+        "\n"
+        + colored(" i ", "blue", attrs=["reverse"]) * (is_windows)
+        + emoji.emojize(":information:") * (not is_windows)
+        + "   Once you have completed the steps, continue:"
+    )
+
+    cont = receive_proceed_action()
+
+    if cont == "1":
+
+        if validate_page_load(driver, html_load) == False:
+            return False
+
+        print("\nChecking for successful login.\n")
+
+        time.sleep(1)
+
+        if validate_login(driver, html_login) == True:
+
+            driver.maximize_window()
+
+            driver.set_window_position(-2024, 2024)
+
+            return True
+
+        else:
+
+            return False
+
+    else:
+
+        driver.close()
+
+        return False
+
+
+def validate_page_load(driver, html_load):
+
+    try:
+
+        WebDriverWait(driver, 60).until(
+            expected_conditions.element_to_be_clickable(
+                (
+                    By.CLASS_NAME,
+                    html_load,
+                )
+            )
+        )
+
+    except:
+
+        print(
+            "\n"
+            + colored(" ! ", "red", attrs=["reverse"]) * (is_windows)
+            + emoji.emojize(":red_exclamation_mark:") * (not is_windows)
+            + colored("  Unable to load JSTOR page.\n", "red")
+        )
+
+        print(
+            "\n"
+            + colored(" i ", "blue", attrs=["reverse"]) * (is_windows)
+            + emoji.emojize(":information:") * (not is_windows)
+            + "  Check your internet connection and try again.\n"
+        )
+
+        driver.close()
+
+        return False
+
+    "pds__access-provided-by"
+
+
+def validate_login(driver, html_login):
+
+    try:
+        driver.find_element(By.CLASS_NAME, html_login)
+
+        time.sleep(1)
+
+        print(
+            "\n"
+            + colored(" ! ", "green", attrs=["reverse"]) * (is_windows)
+            + emoji.emojize(":check_mark_button:") * (not is_windows)
+            + colored("  Login was successful!\n", "green")
+        )
+
+        time.sleep(1)
+
+        return True
+
+    except:
+
+        time.sleep(1)
+
+        print_unsuccessful()
+
+        return False
+
+
+def print_unsuccessful():
+
+    print(
+        "\n"
+        + colored(" ! ", "red", attrs=["reverse"]) * (is_windows)
+        + emoji.emojize(":red_exclamation_mark:") * (not is_windows)
+        + colored("  Login was unsuccessful\n", "red")
+    )
+
+    print(
+        "\n"
+        + colored(" i ", "blue", attrs=["reverse"]) * (is_windows)
+        + emoji.emojize(":information:") * (not is_windows)
+        + "  Try again, make sure you follow the instructions carefully.\n"
+    )
+
+
+def receive_proceed_action():
+
+    proceed_input = get_input(
+        colored(
+            "\n-- Type [1] to continue\n-- Type [2] to return to contributions menu\n   : "
+        )
+    )
+    if proceed_input != "1" and proceed_input != "2":
+
+        typo()
+
+        return receive_proceed_action()
+
+    return proceed_input
+
+
+def receive_end_program_action(driver):
+
+    exit_program = get_input(
+        colored(
+            "\n-- Type [1] to exit\n-- Type [2] to make another contribution\n--Type [3] to go back to main menu   : "
+        )
+    )
+
+    try:
+        restart_count = process_end_program_action(driver, exit_program)
+    except TypoException:
+        return receive_end_program_action(driver)
+
+    return restart_count
+
+
+def process_end_program_action(driver, exit_program):
+
+    if exit_program == "1":
+        driver.close()
+        os._exit(0)
+    elif exit_program == "2":
+        restart_count = 0
+        return restart_count
+    elif exit_program == "3":
+        driver.close()
+        raise MainException
+    else:
+        typo()
+        raise TypoException
+
+
+def get_input(text):
+    return input(text).strip()
